2b55f89c667f298ff2f9104a6fe75c63091c3873
Xiang Wang <ramwin@qq.com>
2024-03-21 00:34:00 +0800

    feat: admin

--- src/sync_model/admin.py
+++ src/sync_model/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SyncTask
+
+
+@admin.register(SyncTask)
+class SyncTaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "source", "target", "sync_method", "batch_size", "order_by", "last_sync", "filter_by"]
